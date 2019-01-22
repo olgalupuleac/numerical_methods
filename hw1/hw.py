e8a67003706c5ec33d91779f09c129baf0dbc793
Olga Lupuleac <olga.lupuleac@gmail.com>
2019-01-23 00:35:23 +0300

    final plots

--- hw1/hw.py
+++ hw1/hw.py
@@ -1,67 +1,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-def A(z):
-    return 1 / np.sqrt(1 + 4 * z)
-
-
-def x1(z):
-    return (1 + np.sqrt(1 + 4 * z)) / 2
-
-
-def x2(z):
-    return (1 - np.sqrt(1 + 4 * z)) / 2
-
-
 eps = 1e-6
 
 
-def is_valid(n, z):
-    return n ** 2 - n - z > 0 and abs(A(z) * np.log((n - x1(z)) / (n - x2(z)))) < eps
-
-
-def binary_search(left, right, z):
-    while right - left > 1:
-        m = (right + left) // 2
-        if is_valid(m, z):
-            right = m
-        else:
-            left = m
-    return right
-
-
-def N(z):
-    n = int(2 * z) + 1
-    while not is_valid(n, z):
-        n *= 2
-    return binary_search(1, n, z)
-
-
-def sum_member(k, z):
+def sum_member_A(k, z):
     return 1 / (k ** 2 - k - z)
 
 
-def w(z):
-    n = N(z)
-    res = 0
-    for k in range(1, n):
-        res += sum_member(k, z)
-    return res
-
-print(np.log((2 - x1(1)) / (2 - x2(1))))
-
-t = np.arange(start=0.01, stop=1.99, step=0.01)
-s = [w(ti) for ti in t]
-
-fig, ax = plt.subplots()
-ax.plot(t, s)
-
-ax.set(xlabel='z', ylabel='wA(z)',
-       title='Первая часть первого задания')
-ax.grid()
-
-fig.savefig("plot1.png")
-plt.show()
+def wA(z):
+    n = int(1 / eps) + 1
+    return sum(map(lambda k : sum_member_A(k, z), range(1, n)))
 
 
+
+
+def sum_member_B(k, z):
+    return z / ((k ** 2 - k - z) * k * (k - 1))
+
+
+def wB(z):
+    n = int((z / (3 * eps)) ** (1 / 3)) + 2
+    return sum(map(lambda k: sum_member_B(k, z), range(2, n))) + 1 - 1 / z
+
+
+def plot(w, file_to_save, title=None, func_name=None):
+    t = np.arange(start=0.01, stop=1.99, step=0.01)
+    s = [w(ti) for ti in t]
+
+
+    fig, ax = plt.subplots()
+    ax.plot(t, s)
+
+    ax.set(xlabel='z', ylabel=func_name,
+           title=title)
+    ax.grid()
+
+    fig.savefig(file_to_save)
+    plt.show()
+
+#plot(wA, 'plot1.png', 'Первая часть первого задания', 'wA(z)')
+#plot(wB, 'plot2.png', 'Вторая часть первого задания', 'wB(z)')
+plot(lambda z : wA(z) - wB(z), 'plot3.png', 'Третья часть первого задания', 'wA(z) - wB(z)')
